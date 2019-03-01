94acefdd848c24fa735ed395e1e63fa383125448
Jake Bladt <jake@jakebladt.com>
2019-03-01 16:47:02 -0500

    +floating-point test

--- proxy.test.js
+++ proxy.test.js
@@ -3,3 +3,9 @@ const proxyImpl = require('./proxy');
 test('Finds stats when no proxy used', () => {
   expect(proxyImpl.batter.hr).toBe(35);
 });
+
+test('Stores batting average as a floating-point number', () => {
+  expect(proxyImpl.batter.ba).toBeCloseTo(0.299);
+  expect(proxyImpl.batter.ba).not.toBeCloseTo(0.3, 4);
+  expect(proxyImpl.batter.ba).not.toBeCloseTo(0.298, 4);
+});
