41105f96a55fca708853379395fd0911b5b173dc
Jake Bladt <jake@jakebladt.com>
2019-03-01 17:26:33 -0500

    +late-assigned proxy values

--- proxy.js
+++ proxy.js
@@ -3,4 +3,37 @@ const batter = {
   hr: 35
 };
 
-module.exports = { batter };
+const caseSensitiveBatter = {
+    BA: 0.287,
+    HR: 29,
+    firstName: 'John'
+};
+
+const caseInsensitveSettingsTraps = (() => {
+    const internalLcStats = [];
+
+    return {
+        get: (obj, prop) => {
+            return prop in obj ? obj[prop] : internalLcStats[prop.toLowerCase()]; 
+        },
+        set: (obj, prop, value) => {
+            internalLcStats[prop.toLowerCase()] = value;
+            obj[prop] = value;
+        }
+    };
+})();
+
+const lateAssignedBatterProxy = new Proxy({}, caseInsensitveSettingsTraps);
+lateAssignedBatterProxy.HR = 9;
+lateAssignedBatterProxy.SB = 22;
+
+const indirectBatterProxy = new Proxy({...caseSensitiveBatter}, caseInsensitveSettingsTraps);
+const directBatterProxy = new Proxy(caseSensitiveBatter, caseInsensitveSettingsTraps);
+
+module.exports = { 
+    batter, 
+    caseSensitiveBatter, 
+    lateAssignedBatterProxy,
+    indirectBatterProxy,
+    directBatterProxy
+ };
--- proxy.test.js
+++ proxy.test.js
@@ -9,3 +9,14 @@ test('Stores batting average as a floating-point number', () => {
   expect(proxyImpl.batter.ba).not.toBeCloseTo(0.3, 4);
   expect(proxyImpl.batter.ba).not.toBeCloseTo(0.298, 4);
 });
+
+test('Case-sensitive stats can not be found via miscasing', () => {
+    expect(proxyImpl.caseSensitiveBatter.HR).toBeDefined();
+    expect(proxyImpl.caseSensitiveBatter.hr).toBeUndefined();
+});
+
+test('After-proxy settings are included in lower-case', () => {
+    expect(proxyImpl.lateAssignedBatterProxy.HR).toBe(9);
+    expect(proxyImpl.lateAssignedBatterProxy.hr).toBe(9);
+    expect(proxyImpl.lateAssignedBatterProxy.Hr).toBe(9);
+});
