df2778c505f26996925bbf563229afa4e3351340
Jake Bladt <jake@jakebladt.com>
2019-03-02 11:37:59 -0500

    first pass at classes

--- tshello/2.3.ts
+++ tshello/2.3.ts
@@ -9,7 +9,7 @@ class DOM {
         this._counter = this._getDOMElement('#counter');
     }
 
-    public _getDOMElement(selector: string) : HTMLElement {
+    private _getDOMElement(selector: string) : HTMLElement {
         return document.querySelector(selector) as HTMLElement;
     }
 
@@ -28,5 +28,40 @@ class DOM {
     set counter(value: number) {
         this._counter.innerText = value.toString();
     }
-
 }
+
+class DOMEvents {
+    public register(htmlElement: HTMLElement, type: string, callback: (e: Event) => void) : void {
+        htmlElement.addEventListener(type, callback);
+    }
+}
+
+class App {
+    constructor(public dom: DOM, public domEvents: DOMEvents) {
+        this.setupEvents();
+    }
+
+    private setupEvents(): void {
+        const buttons = [this.dom.incBtn, this.dom.decBtn];
+        buttons.forEach(b => this.domEvents.register(b, 'click', this.handleClicks.bind(this)));
+    }
+
+    private handleClicks(e: MouseEvent) : void {
+        const {id} = <HTMLElement>e.target;
+        if(id === 'incBtn') {
+            this.incrementCounter();
+        } else {
+            this.decrementCounter();
+        }
+    }
+
+    private incrementCounter() : void {
+        this.dom.counter++;
+    }
+
+    private decrementCounter(): void {
+        this.dom.counter--;
+    }
+}
+
+(new App(new DOM(), new DOMEvents()));
