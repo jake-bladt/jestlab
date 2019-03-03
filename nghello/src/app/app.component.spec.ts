dfe74e5e4fcb0402866aaed7903edcd7a59654da
Jake Bladt <jake@jakebladt.com>
2019-03-03 13:16:15 -0500

    update specs

--- nghello/src/app/app.component.spec.ts
+++ nghello/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nghello'`, () => {
+  it(`should have as title 'Angular hello world app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('nghello');
+    expect(app.title).toEqual('Angular hello world app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to nghello!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular hello world app!');
   });
 });
