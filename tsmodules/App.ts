41b3d932e5dc8c02fb95fbca07fd960a5c493c19
Jake Bladt <jake@jakebladt.com>
2019-03-04 13:23:08 -0500

    +embedded namespaces

--- tsmodules/App.ts
+++ tsmodules/App.ts
@@ -1,4 +1,6 @@
 /// <reference path="DraftNinja.ts" />
 
 const player = new DraftNinja.Player("Bobby Bonilla");
+const rbi = new DraftNinja.Stats.RBI(100);
+
 console.log(player.name);
--- tsmodules/DraftNinja.ts
+++ tsmodules/DraftNinja.ts
@@ -2,4 +2,10 @@ namespace DraftNinja {
     export class Player {
         constructor(public name: string) {}
     }
+
+    export namespace Stats {
+        export class RBI {
+            constructor(public count: number) {}
+        }
+    }
 }
