9d706f6eedaaa701f4eaa3ead5adf11ac112fb41
Jake Bladt <jake@jakebladt.com>
2019-03-07 20:43:49 -0500

    first attempt at partition

--- partition/typescript/partition.ts
+++ partition/typescript/partition.ts
@@ -4,13 +4,15 @@ declare global {
     }
 }
 
-Array.prototype.partition = (f) => {
+Array.prototype.partition = (f, target?:{}) => {
     const _addToObject = (baseObj, key, val) => {
         let prop = baseObj[key];
         if(!Array.isArray(prop)) prop = [];
         prop[val].push(key);
     };
 
+    const accumulator = target || {};
+    return this.reduce(accumulator, e => _addToObject(accumulator, f(e), e));
 }
 
 export {}
