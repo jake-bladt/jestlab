b4388c83698ca3b1713737c04fa4b942605dd628
Jake Bladt <jake@jakebladt.com>
2019-03-07 20:56:27 -0500

    more experiments

--- partition/typescript/partition.ts
+++ partition/typescript/partition.ts
@@ -5,6 +5,8 @@ declare global {
 }
 
 Array.prototype.partition = (f, target?:{}) => {
+    const _this = this;
+
     const _addToObject = (baseObj, key, val) => {
         let prop = baseObj[key];
         if(!Array.isArray(prop)) prop = [];
@@ -12,7 +14,12 @@ Array.prototype.partition = (f, target?:{}) => {
     };
 
     const accumulator = target || {};
-    return this.reduce(accumulator, e => _addToObject(accumulator, f(e), e));
+    return _this.reduce(accumulator, e => _addToObject(accumulator, f(e), e));
 }
 
 export {}
+
+const nums: number[] = [1, 2, 0, 7, 14, 5, 3];
+// const parts = nums.partition(n => n <= 7);
+const red = nums.reduce((_, n) => _ + n, 0);
+console.log(red);
