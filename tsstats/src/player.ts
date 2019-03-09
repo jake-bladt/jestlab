8a019239e68b9c4a3fe6c5d4d9c7ca8dcd95c04a
Jake Bladt <jake@jakebladt.com>
2019-03-09 16:14:16 -0500

    jigger modules

--- tsstats/src/player.ts
+++ tsstats/src/player.ts
@@ -1,4 +1,4 @@
-export namespace baseball {
+export namespace Baseball {
     export interface IPlayer {
         name: string,
         relativeValue: number
--- tsstats/tests/player.test.ts
+++ tsstats/tests/player.test.ts
@@ -1,6 +1,11 @@
 import expect from 'chai';
-import * as baseball from '../src/player';
+import { Baseball } from '../src/player';
+import {} from 'jasmine';
 
 describe('Player', () => {
-
+    it('propogates public fields from the constructor', () => {
+        const player : Baseball.Batter = new Baseball.Batter('John Smith', 45, 22);
+        expect(player.hr).to.equal(45);
+        expect(player.sb).to.equal(22);
+    })
 });
